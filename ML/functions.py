e74ec6e87d7369ba2cec019f907fb6ab51498396
glukhodednastya <nastyag755>
2021-10-26 14:41:29 +0300

    add a description

--- ML/functions.py
+++ ML/functions.py
@@ -1,5 +1,9 @@
 from typing import List
 
+"""
+Подсчитать произведение ненулевых элементов на диагонали прямоугольной матрицы. 
+Если ненулевых элементов на диагонали нет, то вернуть -1.
+"""
 def prod_non_zero_diag(X: List[List[int]]):
     prod = -1
     for i in range(min(len(X), len(X[0]))):
@@ -8,9 +12,16 @@ def prod_non_zero_diag(X: List[List[int]]):
             prod *= X[i][i]
     return prod
 
+"""
+Даны два вектора x и y. Проверить, задают ли они одно и то же мультимножество.
+"""
 def are_multisets_equal(x: List[int], y: List[int]):
     return sorted(x) == sorted(y)
 
+"""
+ Найти максимальный элемент в векторе x среди элементов, перед которыми стоит нулевой. 
+ Если нулевых элементов нет, то вернуть -1.
+"""
 def max_after_zero(x: List[int]):
     max = -1
     for i in range(1, len(x)):
@@ -19,6 +30,11 @@ def max_after_zero(x: List[int]):
                 max = x[i]
     return max
 
+"""
+ Дан трёхмерный массив, содержащий изображение, размера (height, width, num_channels), 
+ а также вектор весов длины num_channels. Сложить каналы изображения с указанными весами, 
+ и вернуть результат в виде матрицы размера (height, width). 
+"""
 def convert_image(image: List[List[List[float]]], weights: List[float]) -> List[List[float]]:
     sum = 0
     r = []
@@ -33,6 +49,11 @@ def convert_image(image: List[List[List[float]]], weights: List[float]) -> List[
         r = []
     return res
 
+"""
+Реализовать кодирование длин серий (Run-length encoding). 
+Для некоторого вектора x необходимо вернуть два вектора одинаковой длины. 
+Первый содержит числа, а второй - сколько раз их нужно повторить.
+"""
 def run_length_encoding(x: List[int]) -> (List[int], List[int]):
     numbers = []
     countNumber = []
@@ -53,6 +74,9 @@ def run_length_encoding(x: List[int]) -> (List[int], List[int]):
     countNumber.append(count)
     return numbers, countNumber
 
+"""
+Даны две выборки объектов - X и Y. Вычислить матрицу евклидовых расстояний между объектами.
+"""
 from scipy.spatial.distance import cdist
 def pairwise_distance(X: List[List[float]], Y: List[List[float]]) -> List[List[float]]:
     return list(cdist(X, Y))
--- ML/functions_vectorised.py
+++ ML/functions_vectorised.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+"""
+Подсчитать произведение ненулевых элементов на диагонали прямоугольной матрицы. 
+Если ненулевых элементов на диагонали нет, то вернуть -1.
+"""
 def prod_non_zero_diag(X: np.ndarray):
     prod = -1
     prod *= np.prod(np.diag(X)[np.diag(X) != 0])
@@ -7,9 +11,16 @@ def prod_non_zero_diag(X: np.ndarray):
         return -1
     return abs(prod)
 
+"""
+Даны два вектора x и y. Проверить, задают ли они одно и то же мультимножество.
+"""
 def are_multisets_equal(x: np.ndarray, y: np.ndarray):
     return np.array_equal(np.sort(x), np.sort(y))
 
+"""
+ Найти максимальный элемент в векторе x среди элементов, перед которыми стоит нулевой. 
+ Если нулевых элементов нет, то вернуть -1.
+"""
 def max_after_zero(x: np.ndarray):
     zero = (x == 0)
     if np.all(x != 0):
@@ -19,9 +30,19 @@ def max_after_zero(x: np.ndarray):
     res = x[zero].max()
     return res
 
+"""
+ Дан трёхмерный массив, содержащий изображение, размера (height, width, num_channels), 
+ а также вектор весов длины num_channels. Сложить каналы изображения с указанными весами, 
+ и вернуть результат в виде матрицы размера (height, width). 
+"""
 def convert_image(image: np.ndarray, weights: np.ndarray) -> np.ndarray:
     return image.dot(weights)
 
+"""
+Реализовать кодирование длин серий (Run-length encoding). 
+Для некоторого вектора x необходимо вернуть два вектора одинаковой длины. 
+Первый содержит числа, а второй - сколько раз их нужно повторить.
+"""
 def run_length_encoding(x: np.ndarray) -> (np.ndarray, np.ndarray):
     x = np.asanyarray(x)
     n = x.shape[0]
@@ -36,5 +57,8 @@ def run_length_encoding(x: np.ndarray) -> (np.ndarray, np.ndarray):
         run_lengths = np.diff(np.append(run_starts, n))
         return run_values, run_lengths
 
+"""
+Даны две выборки объектов - X и Y. Вычислить матрицу евклидовых расстояний между объектами.
+"""
 def pairwise_distance(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
     return np.sqrt(np.add.outer(np.sum(X ** 2, axis=1), np.sum(Y ** 2, axis=1)) - 2 * np.dot(X, Y.T))
